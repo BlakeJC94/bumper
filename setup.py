9bda7c33a2249b1fe93b2d67166a6abba7c906b1
BlakeJC94 <blakejamescook@gmail.com>
2022-11-03 17:12:46 +1100

    reqs: compile

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="quaker",
+    name="bumper",
     version=__version__,
-    description="Lightweight python API to USGS earthquake dataset",
+    description="Version bumper for python projects across many files",
     long_description=long_description,
     author="BlakeJC94",
     author_email="blakejamescook@gmail.com",
-    url="https://github.com/BlakeJC94/quaker",
+    url="https://github.com/BlakeJC94/bumper",
     python_requires=">=3.9",
     packages=find_packages(),
     install_requires=[],
