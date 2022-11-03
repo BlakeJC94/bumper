6bb273c65804892d4ab04b0e8c3e8c98fc4f88ea
BlakeJC94 <blakejamescook@gmail.com>
2022-11-04 08:02:47 +1100

    doc: plans for cli

--- bumper/__main__.py
+++ bumper/__main__.py
@@ -1,5 +1,10 @@
 import argparse
 
+# CLI interface plan:
+# * Optionally specify files ad positional args (setup.py and/or watched files to override)
+# * flags --major --minor --patch, one is required
+
+# bumper will automatically look at git diffs and determine where to bump
 
 def main():
     print("Wumbo")
