d0d5804cabc6a53947abca4807fe422e1909560d
BlakeJC94 <blakejamescook@gmail.com>
2022-11-08 08:24:34 +1100

    ifeat: updated cli parser

--- bumper/cli.py
+++ bumper/cli.py
@@ -1,54 +1,81 @@
 import logging
-from argparse import ArgumentParser, REMAINDER
+from argparse import ArgumentParser
+from glob import glob
 from importlib.metadata import metadata
 from pathlib import Path
-from typing import Optional, List, Any, Dict
+from typing import Optional, List, Dict
 
 logger = logging.getLogger(__name__)
 
+ARGS = [("M", "major"), ("m", "minor"), ("p", "patch")]
 
-def parse_args(args: Optional[List[str]] = None) -> Dict[str, Any]:
+
+def parse_args(args: Optional[List[str]] = None) -> Dict[str, Optional[List[str]]]:
     """Parser for CLI interface that converts command line args to a python dictionary.
 
     Args:
         args: List of args passed into the cli interface (uses `sys.argv` if left as None).
 
     Usage:
-        >>> result = parse_args("--patch foo bar".split())
-        >>> print(result)
-        {'major': False, 'minor': False, 'patch': True, 'files': ['foo', 'bar']}
+        >>> parse_args("-m foo.py bar.py -M bar.py baz.py wumbo.txt".split())
+        {'major': ['bar.py', 'baz.py'], 'minor': ['foo.py'], 'patch': None}
 
     """
     prog = Path(__file__).parent.name
     parser = ArgumentParser(
-        prog=prog,
-        description=metadata(prog)["Summary"],
-        add_help=True,
+        prog=prog, description=metadata(prog)["Summary"], add_help=True, epilog=""
     )
 
-    for bump_long, bump_short in [("major", "M"), ("minor", "m"), ("patch", "p")]:
+    for arg_short, arg_long in ARGS:
         parser.add_argument(
-            "-" + bump_short,
-            "--" + bump_long,
-            action="store_true",
-            help=f"Bump all `__version__` variables by {bump_long} version.",
+            "-" + arg_short,
+            "--" + arg_long,
+            nargs="*",
+            metavar="FILES",
+            help=f"Bump `__version__` in FILES by {arg_long} version.",
         )
-
-    parser.add_argument(
-        "files",
-        nargs=REMAINDER,
-        type=str,
-        default=None,
-        help="Files to bump (automatically searches if no args given).",
-    )
 
     result = vars(parser.parse_args(args))
 
-    if sum(result[var] for var in ["major", "minor", "patch"]) != 1:
-        raise ValueError("One of --major, --minor, --patch must be passed")
+    if all(result[var] is None for _, var in ARGS):
+        parser.parse_args(["-h"])
 
-    invalid_options = [f for f in result['files'] if f.startswith('-')]
-    if len(invalid_options) > 0:
-        raise ValueError(f"Invalid options given: {invalid_options}")
+    all_py_files = _filter_py_files(glob("**/*"))
+    for _, var in ARGS:
+        if result[var] is None:
+            continue
+        result[var] = _filter_py_files(result[var]) if len(result[var]) > 0 else all_py_files
+
+    result["minor"] = _filter_overlaps(result["minor"], result["major"])
+    result["patch"] = _filter_overlaps(result["patch"], result["major"])
+    result["patch"] = _filter_overlaps(result["patch"], result["minor"])
 
     return result
+
+
+def _filter_py_files(files: List[str]) -> List[str]:
+    """Filters a list of filepaths to paths that end with .py
+
+    Usage:
+        >>> filter_py_files([foo.py, bar.pyc, baz.txt])
+        [foo.py]
+        >>> filter_py_files([])
+        []
+        >>> filter_py_files(None)
+        []
+    """
+    if files is None:
+        return []
+    return [fp for fp in set(files) if fp.endswith(".py")]
+
+
+def _filter_overlaps(list_0: List[str], list_1: List[str]) -> List[str]:
+    """Filters overlaps between lists
+
+    Usage:
+        >>> filter_overlaps(['foo', 'bar'], ['bar', 'baz'])
+        ['foo']
+    """
+    if list_0 is None or list_1 is None:
+        return list_0
+    return [fp for fp in list_0 if fp not in list_1]
