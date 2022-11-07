bd2a9b50dcf9dcd08f59e3bce08d6de01c804e51
BlakeJC94 <blakejamescook@gmail.com>
2022-11-07 17:01:57 +1100

    docs: idea for v2 of cli

--- bumper/__main__.py
+++ bumper/__main__.py
@@ -2,5 +2,12 @@ from .cli import parse_args
 # bumper will automatically look at git diffs and determine where to bump
 
 def main():
-    print("Wumbo")
+    result = parse_args()
+    print(result)
+    # If no files specified, determine which files have changed
+    # exclude files that have had __version__ change (globs as well?)
+    # Check overrides from args
+    # replace each __version__
     return 0
+
+    # IDEA: bumper --patch foo bar --minor baz --major wumbo mini
