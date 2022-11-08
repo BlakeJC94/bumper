9d0c0419a5b0672ef37e15ff8bd5e0f85393bba2
BlakeJC94 <blakejamescook@gmail.com>
2022-11-08 22:14:00 +1100

    shore: blacvk

--- bumper/cli.py
+++ bumper/cli.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from argparse import ArgumentParser
 from glob import glob
 from importlib.metadata import metadata
@@ -10,6 +11,8 @@ logger = logging.getLogger(__name__)
 ARGS = [("M", "major"), ("m", "minor"), ("p", "patch")]
 
 
+# TODO: I should check if `setup.py` has __version__...
+# TODO: (Downstream) bump version only if change is detected (something for the GH action?)
 def parse_args(args: Optional[List[str]] = None) -> Dict[str, Optional[List[str]]]:
     """Parser for CLI interface that converts command line args to a python dictionary.
 
@@ -37,48 +40,65 @@ def parse_args(args: Optional[List[str]] = None) -> Dict[str, Optional[List[str]
 
     result = vars(parser.parse_args(args))
 
-    if all(result[var] is None for _, var in ARGS):
-        parser.parse_args(["-h"])
-
-    # TODO update filter to get only files with __version__ strings
-    all_py_files = _filter_py_files(['setup.py', *glob("**/*")])
+    # Filter args
     for _, var in ARGS:
         if result[var] is None:
             continue
-        result[var] = _filter_py_files(result[var]) if len(result[var]) > 0 else all_py_files
 
+        # Set default value (when arg is called with out inputs)
+        if len(result[var]) == 0:
+            result[var] = ["setup.py"]
+
+        # Check for globs that weren't processed
+        processed_globs = []
+        for file in result[var]:
+            if "*" in file:
+                processed_globs.extend(glob(file))
+                result[var].remove(file)
+        result[var].extend(processed_globs)
+
+        # Filter to selected files with versions
+        result[var] = [fp for fp in result[var] if _is_py_file_with_version(fp)]
+
+    # Major presides over minor, which presides over patch
     result["minor"] = _filter_overlaps(result["minor"], result["major"])
     result["patch"] = _filter_overlaps(result["patch"], result["major"])
     result["patch"] = _filter_overlaps(result["patch"], result["minor"])
 
+    # If empty list after filters, replace with None
+    for _, var in ARGS:
+        if len(result[var]) == 0:
+            result[var] = None
+
+    # Print help and exit if all none
+    if all(result[var] is None for _, var in ARGS):
+        parser.parse_args(["-h"])
+
     return result
 
 
-def _filter_py_files(files: List[str]) -> List[str]:
-    """Filters a list of filepaths to paths that end with .py
-
-    Usage:
-        >>> _filter_py_files(['foo.py', 'bar.pyc', 'baz.txt'])
-        ['foo.py']
-        >>> _filter_py_files([])
-        []
-        >>> _filter_py_files(None)
-        []
+def _is_py_file_with_version(fp: str) -> bool:
     """
-    if files is None:
-        return []
-    out = sorted([fp for fp in set(files) if fp.endswith(".py")])
-    return out if len(out) > 0 else None
+    Usage:
+        >>> _is_py_file_with_version('setup.py')
+        True
+        >>> _is_py_file_with_version('doesnt_exist.py')
+        False
+        >>> _is_py_file_with_version('bumper/__init__.py')
+        False
+    """
+    has_version = False
+    if not Path(fp).exists() or not fp.endswith(".py"):
+        return has_version
 
-# def _get_version_str(file: str) -> Optional[str]:
-#     with open(file, 'wb', encoding='utf-8') as f:
-#         data = f.read().split('\n')
-#     try:
-#         version = next(line.strip() for line in data if line.strip().startswith('__version__'))
-#     except StopIteration:
-#         return None
+    with open(fp, "r", encoding="utf-8") as fh:
+        try:
+            _ = next(l for l in fh.readlines() if "__version__=" in l.replace(" ", ""))
+            has_version = True
+        except StopIteration:
+            pass
 
-#     return version.replace("'", '"').str
+    return has_version
 
 
 def _filter_overlaps(list_0: List[str], list_1: List[str]) -> Optional[List[str]]:
@@ -87,8 +107,14 @@ def _filter_overlaps(list_0: List[str], list_1: List[str]) -> Optional[List[str]
     Usage:
         >>> _filter_overlaps(['foo', 'bar'], ['bar', 'baz'])
         ['foo']
+        >>> _filter_overlaps([], ['bar', 'baz'])
+        None
+        >>> _filter_overlaps(['foo', 'bar'], [])
+        ['foo', 'bar']
+        >>> _filter_overlaps([], [])
+        None
     """
     if list_0 is None or list_1 is None:
         return list_0
-    out = sorted([fp for fp in list_0 if fp not in list_1])
-    return out if len(out) > 0 else None
+    out = [fp for fp in list_0 if fp not in list_1]
+    return sorted(out) if len(out) > 0 else None
