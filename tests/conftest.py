f5fb704a733b717fa53a0ea0ebfd62619b2ca40d
BlakeJC94 <blakejamescook@gmail.com>
2022-11-12 18:03:09 +1100

    test: complete bump_file tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,8 @@
 import os
-import logging
 import tempfile
 from pathlib import Path
 
 import pytest
-
-logger = logging.getLogger(__file__)
 
 
 @pytest.fixture(autouse=True)
@@ -13,84 +10,42 @@ def _import_modules_for_doctest(doctest_namespace):
     doctest_namespace["Path"] = Path
 
 
+def create_tmp_file(data):
+    file = tempfile.mkstemp()[1] + ".py"
+    with open(file, "w", encoding="utf-8") as fh:
+        fh.write(data)
+
+    return file
+
+
 @pytest.fixture
-def test_file(version, template):
-    with open(template, "r", encoding="utf-8") as fh:
+def test_file_with_version(version):
+    with open("tests/templates/with_version.py.template", "r", encoding="utf-8") as fh:
         data = fh.read()
 
     version_marker = "%VERSION%"
     if version_marker in data:
         data = data.replace(version_marker, f'"{version}"', 1)
 
-    # setup fixture
-    file = tempfile.mkstemp()[1] + ".py"
-    with open(file, "w", encoding="utf-8") as fh:
-        fh.write(data)
-
+    file = create_tmp_file(data)
     yield file
-
-    # teardown fixture
     os.remove(file)
 
 
-FILE_CONTENTS_WITH_VERSION = """'''Mock file with version'''
-from foo import bar, baz
+@pytest.fixture
+def test_file_without_version_symbol():
+    with open("tests/templates/without_version_symbol.py.template", "r", encoding="utf-8") as fh:
+        data = fh.read()
 
-__version__ = "1.2.3"
-
-class Foo:
-    bar = bar
-    baz = baz
-
-    def __init__(self)
-        self.barbaz = [bar, baz]
-
-if __name__ == "__main__":
-    bar(Foo().barbaz)
-"""
+    file = create_tmp_file(data)
+    yield file
+    os.remove(file)
 
 
 @pytest.fixture
-def file_contents_with_version():
-    return FILE_CONTENTS_WITH_VERSION
-
-
-FILE_CONTENTS_WITHOUT_VERSION_SYMBOL = """'''Mock file without version'''
-from foo import bar, baz
-
-class Foo:
-    bar = bar
-    baz = baz
-
-    def __init__(self)
-        self.barbaz = [bar, baz]
-
-if __name__ == "__main__":
-    bar(Foo().barbaz)
-"""
-
-
-@pytest.fixture
-def file_contents_without_version_symbol():
-    return FILE_CONTENTS_WITHOUT_VERSION_SYMBOL
-
-
-FILE_CONTENTS_WITHOUT_VERSION_DEFINED = """'''Mock file without version'''
-from foo import bar, baz, __version__
-
-class Foo:
-    bar = bar
-    baz = baz
-    version = __version__
-
-    def __init__(self)
-        self.barbaz = [bar, baz]
-
-if __name__ == "__main__":
-    bar(Foo().barbaz)
-"""
-
-
-@pytest.fixture
-def file_contents_without_version_defined():
-    return FILE_CONTENTS_WITHOUT_VERSION_DEFINED
+def test_file_without_version_defined():
+    with open("tests/templates/without_version_symbol.py.template", "r", encoding="utf-8") as fh:
+        data = fh.read()
+    file = create_tmp_file(data)
+    yield file
+    os.remove(file)
--- tests/test_core.py
+++ tests/test_core.py
@@ -8,7 +8,6 @@ import pytest
 from bumper.core import bump_file, get_version, line_has_version
 
 
-@pytest.mark.parametrize("template", ["tests/templates/with_version.py.txt"])
 @pytest.mark.parametrize(
     "version, mode, expected",
     [
@@ -17,23 +16,35 @@ from bumper.core import bump_file, get_version, line_has_version
         ("1.2.3", "major", "2.0.0"),
     ],
 )
-def test_bump_file_with_version(test_file, version, mode, expected, template):
-    # create test file with content TODO put into fixture
-    assert get_version(test_file) == version
-    with open(test_file, "r", encoding="utf-8") as fh:
+def test_bump_file_with_version(test_file_with_version, version, mode, expected):
+    assert get_version(test_file_with_version) == version
+    with open(test_file_with_version, "r", encoding="utf-8") as fh:
         data = fh.read()
 
     # run func without os.remove
     with patch("os.remove", return_value=None) as mock_os_remove:
-        bump_file(test_file, mode)
-        mock_os_remove.assert_called_once_with(test_file + ".bumper")
+        bump_file(test_file_with_version, mode)
+        mock_os_remove.assert_called_once_with(test_file_with_version + ".bumper")
 
     # cleanup temp file after check
-    os.remove(test_file + ".bumper")
+    os.remove(test_file_with_version + ".bumper")
 
     # check results
-    assert get_version(test_file) == expected
-    with open(test_file, "r", encoding="utf-8") as fh:
+    assert get_version(test_file_with_version) == expected
+    with open(test_file_with_version, "r", encoding="utf-8") as fh:
         for original_line, new_line in zip(data.split("\n"), fh.readlines()):
             if not line_has_version(original_line):
                 assert original_line == new_line.replace("\n", "")
+
+
+@pytest.mark.parametrize("mode", ["patch", "minor", "major"])
+def test_bump_file_without_version(
+    test_file_without_version_symbol,
+    test_file_without_version_defined,
+    mode,
+):
+    for test_file in [test_file_without_version_symbol, test_file_without_version_defined]:
+        with patch("os.remove", return_value=None) as mock_os_remove:
+            bump_file(test_file, mode)
+            assert not os.path.exists(test_file + ".bumper")
+            mock_os_remove.assert_not_called()
